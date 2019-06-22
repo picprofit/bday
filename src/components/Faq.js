29bdf1680ec566f0990057155c11fe698cc1415e
Konstantin <picprofit@gmail.com>
2019-06-22 12:09:20 +0300

    Faq updated

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 class Faq extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      faq: {
-        'What\'s this?': 'Just a birthday card with some facts about target\'s age, birthday date and ' +
-        'picture from NASA',
-        'For whom?': 'Just for fun and for me to study React',
-
-      }
-    };
-  }
+  faq = {
+        'What\'s this?': `Just a birthday card with some facts about target's age, birthday date 
+        and picture from NASA`,
+        'For whom?': `Just for fun and for me to study React`,
+        'Can i see the code?': `Sure, here's the 
+<a href="https://github.com/picprofit/bday" target="_blank" rel='noopener noreferrer'>Github repo</a>`,
+      };
 
   render() {
     return <React.Fragment>
@@ -20,8 +16,8 @@ class Faq extends React.Component {
           <div className="col-md-12">
             <h1>FAQ</h1>
             <ul>
-              {Object.keys(this.state.faq).map(
-                key => <li key={key}><p><b>{key}</b><br/>{this.state.faq[key]}</p></li>
+              {Object.keys(this.faq).map(
+                key => <li key={key}><p><b>{key}</b><br/><span dangerouslySetInnerHTML={{__html: this.faq[key]}}></span></p></li>
               )}
             </ul>
           </div>
