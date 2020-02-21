f422c806797ffb9b381a973256ad5c1197b0a067
picprofit <picprofit@gmail.com>
2020-02-21 13:54:52 -0400

    upd: to func component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import db from '../db';
 import Login from '../components/Login';
 import { Link } from 'react-router-dom';
 
-class Cards extends React.Component {
-  state = {
+const Cards = () => {
+  const [state, setState] = useState({
     uid: null,
     cards: {},
-  };
+  });
 
-  renderCardLink = (key) => {
-    const cardLink = `card/${this.state.cards[key]}`;
-    const editLink = `edit/${this.state.cards[key]}`;
+ const renderCardLink = (key) => {
+    const cardLink = `card/${state.cards[key]}`;
+    const editLink = `edit/${state.cards[key]}`;
     return <tr key={key}>
       <td>
-        <Link to={cardLink}>{this.state.cards[key]}</Link>
+        <Link to={cardLink}>{state.cards[key]}</Link>
       </td>
       <td>
         <Link to={editLink}>edit</Link>
@@ -23,53 +23,50 @@ class Cards extends React.Component {
   };
 
   setUid = (uid) => {
-    this.setState({
+    setState({
       uid: uid
     }, this.syncWithDb);
   };
 
   syncWithDb = () => {
     db.syncState(`owners/${this.state.uid}`, {
-      context: this,
+      //context: this, TODO
       state: 'cards'
     });
   };
 
   render() {
-    const cardsKeys = Object.keys(this.state.cards);
-    let Result = "";
-    if (this.state.uid == null) {
-      Result = <React.Fragment>
+    const cardsKeys = Object.keys(state.cards);
+    const Result = this.state.uid == null ? <>
         <h1>Log in please to manage your cards</h1>
-      </React.Fragment>;
-    } else {
-      Result = <React.Fragment>
+      </> : 
+      cardsKeys.length > 0 ? 
+      <>
+                <h1>Your cards</h1>
+                <table className="table table-striped table-hover">
+                  <tbody>
+                  {cardsKeys.map(renderCardLink)}
+                  </tbody>
+                </table>
+              </>
+              :
+      <>
         <h1>No cards found</h1>
         <Link to='/'>
         Try to add one for the beginning?
         </Link>
-      </React.Fragment>;
-      if (cardsKeys.length > 0) {
-        Result = <React.Fragment>
-          <h1>Your cards</h1>
-          <table className="table table-striped table-hover">
-            <tbody>
-            {cardsKeys.map(this.renderCardLink)}
-            </tbody>
-          </table>
-        </React.Fragment>;
-      }
+      </>;
     }
-    return <React.Fragment>
+    return <>
       <div className="container">
         <div className="row">
           <div className="col-md-6 col-md-offset-3">
             {Result}
-            <Login setUid={this.setUid}/>
+            <Login setUid={setUid}/>
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </>
   }
 }
 
