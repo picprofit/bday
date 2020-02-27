17878c386b8d5bccbe6c9089b6b501323b688570
Konstantin <picprofit@gmail.com>
2020-02-27 17:25:16 +0300

    upd: to func component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,70 +1,72 @@
-import React from 'react';
-import Login from '../components/Login';
-import CardForm from '../components/CardForm';
-import {randomId} from '../helpers';
-import db from '../db';
+import React, { useState } from "react";
+import Login from "../components/Login";
+import CardForm from "../components/CardForm";
+import { randomId } from "../helpers";
+import db from "../db";
 
-class Intro extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      uid: this.defaultUid,
-      lastError: ''
-    };
-  }
+const Intro = props => {
+  const defaultUid = "anonym";
+  const [uid, setUid] = useState(defaultUid);
+  const [lastError, setLastError] = useState({
+    hasError: false,
+    errorMessage: ""
+  });
 
-  defaultUid = "anonym";
-
-  setUid = (uid) => {
-    this.setState({
-      uid: uid || this.defaultUid
-    });
-  };
-
-  saveCard = (cardData) => {
+  const saveCard = cardData => {
     const cardId = randomId();
-    let lastError = '';
-    cardData.owner = this.state.uid;
+    cardData.owner = uid;
     db.post(`cards/${cardId}`, {
       data: cardData
-    }).then(() => {
-      db.push(`owners/${this.state.uid}`, {
-        data: [
-          cardId
-        ]
-      }).then(() => {
-        this.props.history.push(`card/${cardId}`);
-      }).catch(error => {
-        lastError = 'Can\'t save owner\'s data';
-        this.setState({lastError});
-        console.log(lastError);
-        console.log(error);
+    })
+      .then(() => {
+        db.push(`owners/${uid}`, {
+          data: [cardId]
+        })
+          .then(() => {
+            props.history.push(`card/${cardId}`);
+          })
+          .catch(error => {
+            setLastError({
+              hasError: true,
+              errorMessage: `Can't save owner's data: ${error}`
+            });
+          });
+      })
+      .catch(error => {
+        setLastError({
+          hasError: true,
+          errorMessage: `Can't save card data: ${error}`
+        });
       });
-    }).catch(error => {
-      lastError = 'Can\'t save card data';
-      this.setState({lastError});
-      console.log(lastError);
-      console.log(error);
-    });
   };
 
-  render() {
-    return <React.Fragment>
+  return (
+    <>
       <div className="container">
         <div className="row">
           <div className="col-md-6 col-md-offset-3 text-center">
             <h2>Create a birthday card in one click</h2>
-            <h3>With a Pic Of The Day from NASA and fun fact about an age and date</h3>
-            <CardForm age="0" birthday={new Date()} name="" from="Anonym" text="Happy birthday!"
-                      button="Create card!" saveCard={this.saveCard} lastError={this.state.lastError}/>
+            <h3>
+              With a Pic Of The Day from NASA and fun fact about an age and date
+            </h3>
+            <CardForm
+              age="0"
+              birthday={new Date()}
+              name=""
+              from="Anonym"
+              text="Happy birthday!"
+              button="Create card!"
+              saveCard={saveCard}
+              error={lastError["errorMessage"]}
+            />
           </div>
           <div className="col-md-6 col-md-offset-3">
-            <Login setUid={this.setUid}/>
+            <Login setUid={setUid} />
           </div>
         </div>
       </div>
-    </React.Fragment>;
-  }
-}
+    </>
+  );
+};
 
 export default Intro;
