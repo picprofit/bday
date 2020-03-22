d459f36fed548fbba1d55e979b09391f80b33858
Konstantin <picprofit@gmail.com>
2020-03-22 11:16:08 +0300

    formatted

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
-import Rebase from 're-base';
-import firebase from 'firebase/app';
-import 'firebase/database';
-import fireBaseConfig from './config';
+import Rebase from "re-base";
+import firebase from "firebase/app";
+import "firebase/database";
+import fireBaseConfig from "./config";
 
 const firebaseApp = firebase.initializeApp(fireBaseConfig);
 
 const db = Rebase.createClass(firebaseApp.database());
 
-export {firebaseApp};
+export { firebaseApp };
 
 export default db;
