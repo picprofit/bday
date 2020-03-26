028aa5f9f7c3f3e93142dd94b8f4563393de76f0
Konstantin <picprofit@gmail.com>
2020-03-26 20:44:29 +0300

    upd: to TS

---
+++ src/components/Faq.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+interface IFaqItems {
+  [question: string]: string;
+}
+
+const faqItems: IFaqItems = {
+  "What's this?": `Just a birthday card with some facts about target's age, birthday date 
+        and picture from NASA`,
+  "For whom?": `Just for fun and for me to study React`,
+  "Can i see the code?": `Sure, here's the 
+<a href="https://github.com/picprofit/bday" target="_blank" rel='noopener noreferrer'>Github repo</a>`
+};
+
+const Faq: React.FC = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>FAQ</h1>
+            <ul>
+              {Object.keys(faqItems).map(key => (
+                <li key={key}>
+                  <p>
+                    <b>{key}</b>
+                    <br />
+                    <span
+                      dangerouslySetInnerHTML={{ __html: faqItems[key] }}
+                    ></span>
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Faq;
