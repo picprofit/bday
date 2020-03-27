f16434dafcd81650e2cb5e848ffe8a1bdec78902
Konstantin <picprofit@gmail.com>
2020-03-27 18:29:15 +0300

    upd: to func component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,95 +1,113 @@
-import React from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import {firebaseApp} from '../db';
+import React, { useState, useEffect } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { firebaseApp } from "../db";
 
-class Login extends React.Component {
-  state = {
+const Login = ({ setUid }) => {
+  const [state, setState] = useState({
     uid: null,
     loaded: false,
     displayName: "",
     error: false,
     errorMessage: ""
-  };
+  });
 
-  componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.authHandler({user});
-      }
-      this.setState({
-        loaded: true
-      });
-    });
-  };
-
-  authenticate = provider => {
+  const authenticate = provider => {
     const authProvider = new firebase.auth[`${provider}AuthProvider`]();
     firebaseApp
       .auth()
       .signInWithPopup(authProvider)
       .then(() => {
-        this.authHandler();
-      }).catch((error) => {
-      this.setState({
-        error: true,
-        errorMessage: error.message
+        authHandler();
+      })
+      .catch(error => {
+        setState({
+          error: true,
+          errorMessage: error.message
+        });
       });
-    });
   };
 
-  authHandler = (authData) => {
+  const authHandler = authData => {
     const user = authData.user;
-    this.setState({
+    setState({
       uid: user.uid,
       displayName: user.displayName,
       error: false
     });
-    this.props.setUid(user.uid);
+    setUid(user.uid);
   };
 
-  logout = async() => {
+  const logout = async () => {
     await firebase.auth().signOut();
-    this.setState({
+    setState({
       uid: null,
       error: false
     });
-    this.props.setUid();
+    setUid();
   };
 
-  render() {
-    let errors = "";
-    if (this.state.error) {
-      errors = <p className="alert alert-warning animage-appear">{this.state.errorMessage}</p>;
-    }
-    if (this.state.loaded) {
-      if (this.state.uid == null) {
-        return <nav className="login">
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        authHandler({ user });
+      }
+      setState({
+        loaded: true
+      });
+    });
+  }, []);
+
+  let errors = "";
+  if (state.error) {
+    errors = (
+      <p className="alert alert-warning animage-appear">{state.errorMessage}</p>
+    );
+  }
+  if (state.loaded) {
+    if (state.uid == null) {
+      return (
+        <nav className="login">
           <p className="alert alert-info">Sign in to manage your cards</p>
           {errors}
-          <button className="google btn btn-default"
-                  onClick={() => this.authenticate('Google')}>Login with Google
+          <button
+            className="google btn btn-default"
+            onClick={() => authenticate("Google")}
+          >
+            Login with Google
           </button>
-          <button className="facebook btn btn-default"
-                  onClick={() => this.authenticate('Facebook')}>Login with Facebook
+          <button
+            className="facebook btn btn-default"
+            onClick={() => authenticate("Facebook")}
+          >
+            Login with Facebook
           </button>
-          <button className="github btn btn-default"
-                  onClick={() => this.authenticate('Github')}>Login with Github
+          <button
+            className="github btn btn-default"
+            onClick={() => authenticate("Github")}
+          >
+            Login with Github
           </button>
-        </nav>;
-      } else {
-        return <React.Fragment>
+        </nav>
+      );
+    } else {
+      return (
+        <React.Fragment>
           <div className="alert alert-light" role="alert">
-            Logged in as {this.state.displayName}
-            <button className="bn btn-primary btn-sm btn-login" onClick={this.logout}>Logout
+            Logged in as {state.displayName}
+            <button
+              className="bn btn-primary btn-sm btn-login"
+              onClick={logout}
+            >
+              Logout
             </button>
           </div>
         </React.Fragment>
-      }
-    } else {
-      return null;
+      );
     }
-  };
-}
+  } else {
+    return null;
+  }
+};
 
 export default Login;
