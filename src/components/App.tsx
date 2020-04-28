b5908696110f40f36c90a00ef12ae1758f146591
Konstantin <picprofit@gmail.com>
2020-04-28 20:11:10 +0300

    upd: to func component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,22 +10,20 @@ import Cards from "./Cards";
 import EditCard from "./EditCard";
 import Footer from "./Footer";
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Switch>
-          <Route exact path="/" component={Intro} />
-          <Route exact path="/faq" component={Faq} />
-          <Route exact path="/cards/" component={Cards} />
-          <Route path="/card/:cardId" component={Card} />
-          <Route path="/edit/:cardId" component={EditCard} />
-          <Route component={NotFound} />
-        </Switch>
-        <Footer />
-      </React.Fragment>
-    );
-  }
-}
+const App: React.FC = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Intro} />
+        <Route exact path="/faq" component={Faq} />
+        <Route exact path="/cards/" component={Cards} />
+        <Route path="/card/:cardId" component={Card} />
+        <Route path="/edit/:cardId" component={EditCard} />
+        <Route component={NotFound} />
+      </Switch>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
