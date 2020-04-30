04aeb79f254c8a2aaf1c334f6d7a730d5eb0aaad
Konstantin <picprofit@gmail.com>
2020-04-30 19:40:11 +0300

    upd: links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,30 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+interface IFooterLinks {
+  url: string;
+  text: string;
+  blank?: boolean;
+}
+const footerLinks: IFooterLinks[] = [
+  {
+    url: '/',
+    text: 'New card'
+  },
+  {
+    url: '/cards',
+    text: 'My cards'
+  },
+  {
+    url: '/faq',
+    text: 'FAQ'
+  },
+  {
+    url: 'https://github.com/kskonovalov/bday',
+    text: 'Source on GitHub',
+    blank: true
+  }
+];
+
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
       <div className="container">
         <div className="col-md-6 text-right">
           <ul className="list-inline footer-links">
-            <li>
-              <Link to="/">New card</Link>
-            </li>
-            <li>
-              <Link to="/cards">My cards</Link>
-            </li>
-            <li>
-              <Link to="/faq">faq</Link>
-            </li>
-            <li>
-              <a
-                href="https://github.com/picprofit/bday"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Source on GitHub
-              </a>
-            </li>
+            {footerLinks.map((item) => {
+              if(item.blank) {
+                return <li id={item.url}>
+                  <a
+                      href={item.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                    {item.text}
+                  </a>
+                </li>;
+              }
+              return <li id={item.url}>
+                <Link to={item.url}>{item.text}</Link>
+              </li>;
+            })}
           </ul>
         </div>
       </div>
